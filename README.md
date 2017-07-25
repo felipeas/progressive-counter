096b3adfbacf4ef6094a5869d16150b7d7b85fc8
fsilva <felipeas89@gmail.com>
2017-07-25 15:13:30 -0300

    Updates

--- README.md
+++
@@ -1,5 +0,0 @@
-## lifecounter
-
-A simple life counter to mtg games
-
-using create-react-app and bulma
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import list from './list'
-
-const lifeCounter = combineReducers({
-  list
-})
-
-export default lifeCounter
